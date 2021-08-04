dfd085798527057341fda8d36bbb5496df9f8bdc
JonasGraul <jonasgraul@hotmail.com>
2021-08-04 23:48:52 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Storage.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Storage {
+
+
+    public Storage() {
+
+    }
+
+    @PostConstruct
+    public void checkStorage() {
+
+    }
+}
