5e9aa5f9cc06c82742df7f301ebb7b0d3548958a
JonasGraul <jonasgraul@hotmail.com>
2021-08-21 23:39:52 +0200

    Cleanup

--- src/main/java/com/example/demo/ExchangeRateController.java
+++ src/main/java/com/example/demo/ExchangeRateController.java
@@ -7,12 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
-import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 
 @RestController
